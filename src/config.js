2c8b753e39356f6400780104a0d2f87b2e8e7c8b
lisalisa122051-droid <lisalisa122051@gmail.com>
2025-11-19 22:40:09 +0800

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const CONFIG = {
+  STORE_NAME: "LIVIAA ID 2025",
+  ADMINS: ["6281234567890@s.whatsapp.net"],
+  PREFIX: "!",
+  AUTH_FILE: "./auth_info.json",
+  DB_FILE: "./src/db.json",
+  PORT: process.env.PORT || 3000
+};
